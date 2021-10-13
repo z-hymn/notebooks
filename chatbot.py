b8ce71bb5ebf4681449cc3e794c1b035c2503dfb
z-hymn <83352832+z-hymn@users.noreply.github.com>
2021-10-13 10:41:17 -0400

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+#Chatbot for Korean-English speaker. Chatbot named Judahbot
+
+print('안녕하세요. 저는 Judahbot이라고합니다. 저는 chatbot입니다.')
+print('저는 아빠, 엄마 and 고구마 byte을 아주 좋아합니다.')
+name = input('이름 어떻게 되시나요?:')
+print('Hello', name, ', nice to meet you')
+dinner = input('What would you like to eat for dinner tonight?')
+print('That sounds like a wonderful idea! I am excited to eat', dinner,'.')
+
